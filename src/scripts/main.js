e5d4f114edbc624383929cce5b493e1f725378d2
Dmytro Petrov <sidodji228007@gmail.com>
2025-08-24 12:09:43 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,47 @@
 'use strict';
+
+const image1 = document.querySelector(
+  'a > img[src^="/landscape-first-thumb"]',
+)?.parentElement;
+
+const image2 = document.querySelector(
+  'a > img[src^="/landscape-second-thumb"]',
+)?.parentElement;
+
+const image3 = document.querySelector(
+  'a > img[src^="/landscape-third-thumb"]',
+)?.parentElement;
+
+const image4 = document.querySelector(
+  'a > img[src^="/landscape-fourth-thumb"]',
+)?.parentElement;
+
+const image5 = document.querySelector(
+  'a > img[src^="/landscape-fifth-thumb"]',
+)?.parentElement;
+
+image1.addEventListener('click', () => {
+  document.querySelector('#largeImg').src = '../images/landscape-first.png';
+});
+
+image2.addEventListener('click', () => {
+  document.querySelector('#largeImg').src = '../images/landscape-second.png';
+});
+
+image3.addEventListener('click', () => {
+  document.querySelector('#largeImg').src = '../images/landscape-third.png';
+});
+
+image4.addEventListener('click', () => {
+  document.querySelector('#largeImg').src = '../images/landscape-fourth.png';
+});
+
+image5.addEventListener('click', () => {
+  document.querySelector('#largeImg').src = '../images/landscape-fifth.png';
+});
+
+image1.setAttribute('href', '#');
+image2.setAttribute('href', '#');
+image3.setAttribute('href', '#');
+image4.setAttribute('href', '#');
+image5.setAttribute('href', '#');
