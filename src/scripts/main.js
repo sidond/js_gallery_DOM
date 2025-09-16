43d39fe787d1c05d299164669f6b434f2104c4c2
Dmytro Petrov <sidodji228007@gmail.com>
2025-09-16 12:35:15 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,9 +4,12 @@ const ul = document.querySelector('#thumbs');
 
 // eslint-disable-next-line no-shadow
 ul.addEventListener('click', (event) => {
-  event.preventDefault();
-
   const link = event.target.closest('.list-item__link');
 
+  if (!link) {
+    return;
+  }
+
+  event.preventDefault();
   document.querySelector('#largeImg').src = link.getAttribute('href');
 });
